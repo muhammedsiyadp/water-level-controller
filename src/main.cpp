d1d26eeda2c20be6046bd195a03594a90a46c315
muhammedsiyadp <muhammedsiyadofficial@gmail.com>
2024-12-09 17:22:02 +0530

    added dry run functoinaliity

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+//constants
+unsigned long dryrun_timeout = 60; //seconds
 //Pinout
 #define LED_MOTOR_ON_PIN        14
 #define LED_DRY_RUN_CUTOFF_PIN  16
@@ -17,6 +19,10 @@
 #define VOLTAGE_SENSOR_PIN      A0
 
 bool motor_running = false; //global variable to check the motor if it is currently pumping or not
+int motor_running_status = 0; // 0 for not running, 1 for just started( in this case dryrun is not checked), 2 running phase (check for dryrun)
+
+unsigned long current_millis;
+unsigned long starting_millis;
 
 void setup(){
   //setting up Pin Modes
@@ -42,10 +48,13 @@ void setup(){
 }
 void start_motor(){
   motor_running = true;
+  motor_running_status = 1;
   digitalWrite(RELAY_MOTOR_PIN,HIGH);
 }
 void stop_motor(){
   motor_running = false;
+  motor_running_status = 0;
+  starting_millis = millis();
   digitalWrite(RELAY_MOTOR_PIN,LOW);
 }
 int check_voltage(){
@@ -55,11 +64,31 @@ void loop(){
   bool water_low = digitalRead(WATERLEVEL_LOW_PIN);
   bool water_full = digitalRead(WATERLEVEL_FULL_PIN);
   bool motor_dry_run = digitalRead(WATERLEVEL_DRYRUN_PIN);
+
+
+  if (!motor_running){
+    if (!water_low && !water_full){
+      start_motor();
+    }
+    else if (!water_low && water_full){
+      //error condition
+    }
+  }
+  else {  //motor is running
+    if (water_full){
+      stop_motor();
+    }
+    else if (motor_running_status == 1) {
+      if (millis() - starting_millis >= dryrun_timeout){
+        motor_running_status = 2;
+      }
+    }
+    else if (motor_running_status == 2){
+      if (!motor_dry_run){
+        stop_motor();
+      }
+    }
+  }
   
-  if (!water_low && !water_full && !motor_running){
-    start_motor();
-  }
-  else if (water_full && motor_running) {
-    stop_motor();
-  }
+
 }
